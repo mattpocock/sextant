ec44b676ae9f8975e627ba8dadcb35f1ec21b0d2
Matt <mpocock@me.com>
2020-10-30 19:59:32 +0000

    Fixed #3 by ensuring directory always exists

--- packages/frontend/src/bin/utils/saveDatabase.ts
+++ packages/frontend/src/bin/utils/saveDatabase.ts
@@ -1,10 +1,15 @@
 import { Database, flattenDatabase } from "@sextant-tools/core";
 import { buildCodeForCreateService } from "@sextant-tools/adapter-typescript";
 import { writeFileSync } from "fs";
+import * as fsExtra from "fs-extra";
 import { getTargetDatabaseFile, getTargetDir } from "./getTargetDir";
 import * as path from "path";
 
 export const saveDatabase = async (database: Database): Promise<Database> => {
+  const targetDir = getTargetDir();
+
+  fsExtra.ensureDirSync(targetDir);
+
   writeFileSync(getTargetDatabaseFile(), JSON.stringify(database, null, 2));
 
   const filesToWrite = buildCodeForCreateService(flattenDatabase(database));
