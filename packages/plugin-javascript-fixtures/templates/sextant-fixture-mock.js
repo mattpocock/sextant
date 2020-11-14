5cfa273a1b070ad4709c0b3ff24c28625402391d
Matt <mpocock@me.com>
2020-11-14 11:36:34 +0000

    Made the mock immutable

--- packages/plugin-javascript-fixtures/templates/sextant-fixture-mock.js
+++ packages/plugin-javascript-fixtures/templates/sextant-fixture-mock.js
@@ -2,7 +2,8 @@ const mocks = require('./sextant-mocks.generated.json');
 
 const mockSextantEvent = (featureName, eventName) => {
   if (mocks[featureName] && mocks[feature][eventName]) {
-    return mocks[featureName][eventName];
+    // Create a new object to prevent mutable errors
+    return Object.assign({}, mocks[featureName][eventName]);
   }
   throw new Error(`Event ${eventName} not found in ${featureName}`);
 };
